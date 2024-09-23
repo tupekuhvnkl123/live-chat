162183437d19086dc1e93de1ea94a7ab1457eb5e
ofek levi <ofeklevi36@gmail.com>
2024-09-23 13:27:11 +0300

    url fixed

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -1,5 +1,5 @@
 export const fetchMessages = async () => {
-  const response = await fetch("http://localhost:5000/messages");
+  const response = await fetch("https://live-chat-e904.onrender.com/messages");
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
--- front/src/utils/socket.js
+++ front/src/utils/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://live-chat-e904.onrender.com");
 
 export default socket;
